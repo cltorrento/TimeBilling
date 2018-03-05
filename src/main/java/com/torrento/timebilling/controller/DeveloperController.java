ff7426933cf1217c821b52b81f8bf66f2feac5e4
Carlos Torrento <cltorrento@gmail.com>
2018-03-05 12:18:59 +0100

    Updated Controller File and JsonTB.png

--- src/main/java/com/torrento/timebilling/controller/DeveloperController.java
+++ src/main/java/com/torrento/timebilling/controller/DeveloperController.java
@@ -2,14 +2,24 @@ package com.torrento.timebilling.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.torrento.timebilling.model.Developer;
 import com.torrento.timebilling.repository.DeveloperRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class DeveloperController {
@@ -21,4 +31,42 @@ public class DeveloperController {
 	public List<Developer> getAllDevs(){
 		return developerRepository.findAll();
 	}
+	
+	@PostMapping("/devs")
+	public Developer createDeveloper(@Valid @RequestBody Developer developer){
+		return developerRepository.save(developer);
+	}
+	
+	@GetMapping("/devs/{id}")
+	public ResponseEntity<Developer> getDeveloperById(@PathVariable(value = "id") Long developerId) {
+		Developer developer = developerRepository.findOne(developerId);
+		if(developer == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok().body(developer);
+	}
+	
+	@PutMapping("/devs/{id}")
+	public ResponseEntity<Developer> updateDeveloper(@PathVariable(value = "id") Long developerId, @Valid @RequestBody Developer developerDetails) {
+	    Developer developer = developerRepository.findOne(developerId);
+	    if(developer == null) {
+	        return ResponseEntity.notFound().build();
+	    }
+	    developer.setName(developerDetails.getName());
+	    developer.setLastname(developerDetails.getLastname());
+
+	    Developer updatedDeveloper = developerRepository.save(developer);
+	    return ResponseEntity.ok(updatedDeveloper);
+	}
+	
+	@DeleteMapping("/devs/{id}")
+	public ResponseEntity<Developer> deleteDeveloper(@PathVariable(value = "id") Long developerId) {
+	    Developer developer = developerRepository.findOne(developerId);
+	    if(developer == null) {
+	        return ResponseEntity.notFound().build();
+	    }
+	    developerRepository.delete(developer);
+	    return ResponseEntity.ok().build();
+	}
 }
+
